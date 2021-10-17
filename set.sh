164e4054f7dcf59df5b20696d899fa0b3504f153
koki <cohky.github@gmail.com>
2021-10-17 15:25:44 +0900

    add shell

---
+++ set.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -eu
+
+dir=$1
+url=$2
+path=${url##*/}
+contents=${path%_*}
+level=${path#*_}
+mkdir -p $dir/$contents/$level && cd $dir/$contents/$level
+touch main.py && oj d $url
