93e2897b211830187f19ee1e26f40e966cf80566
koki <cohky.github@gmail.com>
2021-10-28 23:31:16 +0900

    add Training

---
+++ Training/abc151/c/main.py
@@ -0,0 +1,15 @@
+n,m=map(int,input().split())
+l = [0]*n
+pe=[0]*n
+r=0
+for _ in range(m):
+    a,p=input().split()
+    i = int(a) - 1
+    if l[i] == 0:
+        if p == "AC":
+            if pe[i] > 0:
+                r += pe[i]
+            l[i] = 1
+        elif p == "WA":
+            pe[i] += 1
+print(sum(l), r)
