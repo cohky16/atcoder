661c63e2310ff8dcfa2970321ce33ac1c304d368
koki <cohky.github@gmail.com>
2021-11-18 23:43:22 +0900

    add Training

---
+++ Training/abc157/c/main.py
@@ -0,0 +1,17 @@
+n, m = map(int, input().split())
+l = list([list(map(int, input().split())) for i in range(m)])
+r = 0
+for ans in range(1000):
+    s = str(ans)
+    if len(s) != n: continue
+    f = True
+    for i in l:
+        for j in range(n):
+            if j == i[0] - 1 and int(s[j]) != i[1]: f = False
+    if f:
+        print(ans)
+        exit()
+print(-1)
+
+
+
