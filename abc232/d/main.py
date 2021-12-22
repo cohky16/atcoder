46699ce47b68321165d9d561828dd840170eef40
koki <cohky.github@gmail.com>
2021-12-22 09:23:36 +0900

    1222

--- abc232/d/main.py
+++ abc232/d/main.py
@@ -1,3 +1,8 @@
 h,w = map(int, input().split())
 clist = [input() for _ in range(h)]
-print(clist)
+dp = [[0]*(w + 1) for _ in range(h + 1)] # グリッドの外も含む
+for i in reversed(range(h)):
+    for j in reversed(range(w)):
+        if clist[i][j] == "#": continue
+        dp[i][j] = max(dp[i + 1][j], dp[i][j + 1]) + 1
+print(dp[0][0])
