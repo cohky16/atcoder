60c138258b7b952211d61e36e5fb01e56d09bd1f
koki <cohky.github@gmail.com>
2022-04-21 22:41:07 +0900

    0421

--- abc248/c/main.py
+++ abc248/c/main.py
@@ -1,6 +1,18 @@
-import math
-n,m,k = map(int, input().split())
-ans = 0
-for i in range(m,k):
-    ans += math.factorial(i)%998244353
-print(ans%998244353)
+MOD = 998244353
+
+def main():
+    N, M, K = map(int, input().split())
+    dp = [[0] * (K + 1) for _ in range(N + 1)]
+    dp[0][0] = 1
+
+    for i in range(N):
+        for j in range(K):
+            for k in range(1, M + 1):
+                if j + k > K:
+                    break
+                dp[i + 1][j + k] += dp[i][j]
+                dp[i + 1][j + k] %= MOD
+    print(sum(dp[-1]) % MOD)
+
+if __name__ == '__main__':
+    main()
